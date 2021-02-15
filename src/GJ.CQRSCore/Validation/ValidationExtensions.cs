62906c9e99055531fd532ca1079ba60d68f87371
Gertjan Gielen <gjgielen@gmail.com>
2021-02-15 22:46:17 +0100

    Add validation extension methods

---
+++ src/GJ.CQRSCore/Validation/ValidationExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace GJ.CQRSCore.Validation
+{
+    public static class ValidationExtensions
+    {
+        public static void ValidateNotNull(this ValidationResults results, int property, string propertyName)
+        {
+            if (property == -1)
+            {
+                results.AddValidationResult(propertyName, "{0} cannot be null");
+            }
+        }
+        public static void ValidateNotNullOrEmpty(this ValidationResults results, string property, string propertyName)
+        {
+            if (string.IsNullOrEmpty(property))
+            {
+                results.AddValidationResult(propertyName, "{0} cannot be null or empty");
+            }
+        }
+    }
+}
---
+++ src/GJ.CQRSCoreTests/Validation/ValidationExtensionTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using GJ.CQRSCore.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GJ.CQRSCore.UnitTests.Validation
+{
+    [TestClass]
+    public class ValidationExtensionTests
+    {
+        [TestMethod]
+        public void TestValidateNotNullWithNotNullValueShouldReturnNoValidationResults()
+        {
+            // Arrange
+
+            var validationResults = new ValidationResults();
+            int input = 1;
+
+            // Act
+
+            validationResults.ValidateNotNull(input, "input");
+
+            // Assert
+
+            validationResults.GetMessages().Should().Be("");
+        }
+
+        [TestMethod]
+        public void TestValidateNotNullWithNullValueShouldReturnValidationResults()
+        {
+            // Arrange
+
+            var validationResults = new ValidationResults();
+            int input = -1;
+
+            // Act
+
+            validationResults.ValidateNotNull(input, "input");
+
+            // Assert
+
+            validationResults.GetMessages().Should().Be("input cannot be null");
+        }
+
+        [TestMethod]
+        public void TestValidateNotNullOrEmptyWithNotNullOrEmptyValueShouldReturnNoValidationResults()
+        {
+            // Arrange
+
+            var validationResults = new ValidationResults();
+            string input = "Test";
+
+            // Act
+
+            validationResults.ValidateNotNullOrEmpty(input, "input");
+
+            // Assert
+
+            validationResults.GetMessages().Should().Be("");
+        }
+
+        [TestMethod]
+        public void TestValidateNotNullOrEmptyWithNullValueShouldReturnValidationResults()
+        {
+            // Arrange
+
+            var validationResults = new ValidationResults();
+            string input = null;
+
+            // Act
+
+            validationResults.ValidateNotNullOrEmpty(input, "input");
+
+            // Assert
+
+            validationResults.GetMessages().Should().Be("input cannot be null or empty");
+        }
+
+        [TestMethod]
+        public void TestValidateNotNullOrEmptyWithEmptyValueShouldReturnValidationResults()
+        {
+            // Arrange
+
+            var validationResults = new ValidationResults();
+            string input = "";
+
+            // Act
+
+            validationResults.ValidateNotNullOrEmpty(input, "input");
+
+            // Assert
+
+            validationResults.GetMessages().Should().Be("input cannot be null or empty");
+        }
+    }
+}
